f4b0ff3bedcf29bf9c9c0ba9218cb3f07e278e91
Harryogita <52612145+Harryogita@users.noreply.github.com>
2020-02-18 16:28:53 +0530

    Create pythonscript.py

---
+++ pythonscript.py
@@ -0,0 +1,6 @@
+#! usr / bin / python3
+print("Hello World ! welcome you all in docker or contenerization platform")
+print("This is a demo program for demonstrating a simple python script runnning inside container")
+print("However python script is placed on my host Os , while spin up container it gets copied from host  to inside container")
+print("Enjoy the script output")
+
